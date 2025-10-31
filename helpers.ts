a3cf7fa4612c37c6c556dfb8fc92e018d15367b7
starceps <98521596+Starceps@users.noreply.github.com>
2025-10-31 16:46:58 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T16:43:46.630Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-10-31T13:46:58.458Z
+Commit: Resolve null pointer exception in user service
